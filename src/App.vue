369da02f95661ca79f450fb63cd1123d4ae47b55
Koen De Groot <koen.de.groot@organimmo.be>
2023-07-26 17:03:05 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<p>hello</p>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+﻿<script setup lang="ts">
+const name = 'Home'
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
