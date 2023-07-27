03cb047323d0ffff0aadead6abf774f07ec83ff4
Koen De Groot <koen.de.groot@organimmo.be>
2023-07-27 17:08:15 +0200

    Enhanced links section with new styles and UI elements
    Updated the 'Links.vue' view by introducing a new design, that includes a header, a subtitle, and a 'Add new link' button. This modification is aimed towards providing a better user experience and to allow users to customize their links. Also, updated 'reset.scss' file increases accessibility by making buttons more user-friendly with the implementation of cursor style.

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -2,8 +2,17 @@
 </script>
 
 <template>
-  <div class="links">
-    <p>Links</p>
+  <div class="links-section">
+    <div class="header">
+      <h1>Customize your links</h1>
+      <p class="subtitle">
+        Add/edit/remove links below and then share all your profiles with the world!
+      </p>
+      <button class="add-new-link-btn">
+        + Add new link
+      </button>
+    </div>
+
   </div>
 </template>
 
@@ -11,8 +20,39 @@
 @import '@/assets/sass/mixins.scss';
 @import '@/assets/sass/variables.scss';
 
-.links{
+.links-section {
   background-color: $white;
   border-radius: 12px;
+  padding: 2.5rem;
+
+  .header {
+    h1 {
+      font-size: 2rem;
+      color: $darkGrey;
+      font-weight: 700;
+    }
+
+    .subtitle {
+      font-size: 1rem;
+      color: $grey;
+      margin-bottom: 2.5rem;
+      font-weight: 400;
+    }
+
+    .add-new-link-btn {
+      padding: 0.6875rem 1.6875rem;
+      text-align: center;
+      border-radius: 8px;
+      border: 1px solid $purple;
+      color: $purple;
+      width: 100%;
+      @include transition;
+      margin-bottom: 1.5rem;
+
+      &:hover {
+        background: $lightPurple;
+      }
+    }
+  }
 }
 </style>
