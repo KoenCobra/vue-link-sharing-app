f187cc7f52ee19b70a36917191940eeff6119782
Koen De Groot <koen.de.groot@organimmo.be>
2023-08-03 14:07:47 +0200

    Persist profile links to local storage
    Previously, profile links were not stored and thus, data loss occurred upon page refresh. This update ensures that profile links are now persisted in local storage. This was accomplished by utilizing JSON.stringify to write the profileLinks value to local storage whenever a profile link is added, updated, or removed.

--- src/stores/profile-link.ts
+++ src/stores/profile-link.ts
@@ -3,30 +3,32 @@ import { ref } from 'vue'
 import type { ProfileLink } from '@/interfaces/profile-link'
 
 export const useProfileLinkStore = defineStore('profileLink', () => {
-  const profileLinks = ref<ProfileLink[]>([])
+  const profileLinks = ref<ProfileLink[]>(JSON.parse(localStorage.getItem('profileLinks') || '[]'))
   const idNumb = ref(1)
 
   function addProfileLink(profileLink: ProfileLink, background: string) {
-    profileLinks.value?.push({
+    profileLinks.value.push({
       id: idNumb.value++,
       platform: profileLink,
       url: '',
       background: background
     })
+    localStorage.setItem('profileLinks', JSON.stringify(profileLinks.value))
   }
 
   function updateProfileLink(profileLink: ProfileLink, id: number) {
     const linkToUpdate = profileLinks.value.find((link) => link.id === id)
-    console.log(linkToUpdate)
 
     if (linkToUpdate) {
       linkToUpdate.url = profileLink.url
       linkToUpdate.platform = profileLink.platform
+      localStorage.setItem('profileLinks', JSON.stringify(profileLinks.value))
     }
   }
 
   function removeProfileLink(linkIndex: any) {
-    profileLinks.value?.splice(linkIndex, 1)
+    profileLinks.value.splice(linkIndex, 1)
+    localStorage.setItem('profileLinks', JSON.stringify(profileLinks.value))
   }
 
   return { profileLinks, addProfileLink, removeProfileLink, updateProfileLink }
