c8b8dfe6d6e2726b5ad6b4d134c4937390398401
Koen De Groot <koen.de.groot@organimmo.be>
2023-08-04 17:13:51 +0200

    Update Auth0 loading UI and callback URL
    In src/App.vue, updated the `isLoading` logic to display a centered loading message instead of plain text, which improves user experience during authentication. In addition, the callback URL in .env has been changed from localhost to the netlify app URL, enabling successful authentication redirect in the deployed version of the app.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup lang="ts">
-import {useAuth0} from "@auth0/auth0-vue";
+import { useAuth0 } from '@auth0/auth0-vue'
 
-const {isLoading} = useAuth0()
-
+const { isLoading } = useAuth0()
 </script>
 
 <template>
-  <div v-if="isLoading">
-    loading...
-  </div>
+  <div class="loading" v-if="isLoading">loading...</div>
   <main v-else>
     <component :is="$route.meta.layout || 'div'">
-      <RouterView/>
+      <RouterView />
     </component>
   </main>
 </template>
 
 <style>
-
+.loading {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
